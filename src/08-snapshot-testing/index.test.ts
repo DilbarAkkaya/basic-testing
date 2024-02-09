9656e1b46149dd97fd0e22e90ed3eba86af60980
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-09 13:17:56 +0300

    feat: implement snapshot testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,50 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
-  });
+  const elements = ['a', 'b', 1, []];
+  const linkedList = {
+    value: 'a',
+    next: {
+      value: 'b',
+      next: {
+        value: 1,
+        next: {
+          value: [],
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    },
+  };
 
-  // Check match by comparison with snapshot
+  const elementsSnap = [1, 2, [], { a: 'b' }];
+
+  test('should generate linked list from values 1', () => {
+    expect(generateLinkedList(elements)).toStrictEqual(linkedList);
+  });
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elementsSnap)).toMatchInlineSnapshot(`
+      {
+        "next": {
+          "next": {
+            "next": {
+              "next": {
+                "next": null,
+                "value": null,
+              },
+              "value": {
+                "a": "b",
+              },
+            },
+            "value": [],
+          },
+          "value": 2,
+        },
+        "value": 1,
+      }
+    `);
   });
 });
